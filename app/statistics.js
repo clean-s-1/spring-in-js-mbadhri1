9cdde0a31c3fe0a8628b6c74d27d6bd00cdbd49b
Badhrinarayanan <badhrinarayanan.m@siemens.com>
2022-05-09 15:09:47 +0530

    average function added

--- app/statistics.js
+++ app/statistics.js
@@ -1,5 +1,9 @@
 module.exports=function getStatistics(numbers){
-    
+    let EmailAlert;
+    const min = Math.min( ...numbers );
+    const max = Math.max( ...numbers );
+    const average = numbers => numbers.reduce( ( p, c ) => p + c, 0 ) / numbers.length;
     //implement the computation of statistics here
+    return {min,max,average}
 }
 
